e02f537f02684dc229cee2af6562ebb94c311940
Dexter <dexter.lente18@gmail.com>
2024-08-29 14:22:50 +0800

    update

---
+++ bitcoin/user_activity_analysis/extract.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def extract_unique_address_used():
+    url = "https://api.blockchain.info/charts/market-price?timespan=30days&sampled=true&metadata=false&daysAverageString=1d&cors=true&format=json"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        values = data.get("values", [])
+        if values:
+            first_y = int(values[0].get("y"))
+            last_y = int(values[-1].get("y"))
+
+            print(f"Last month active unique address {first_y}")
+            print(f"This month  active unique address {last_y}")
+        else:
+            print("No values found in the response.")
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+
+extract_unique_address_used()
