c60c95e70d006c6e0c35146150d521b720587ca2
Dexter <dexter.lente18@gmail.com>
2024-08-29 14:37:15 +0800

    added func

--- bitcoin/user_activity_analysis/extract.py
+++ bitcoin/user_activity_analysis/extract.py
@@ -21,4 +21,25 @@ def extract_unique_address_used():
         print(f"Request failed with status code {response.status_code}")
 
 
-extract_unique_address_used()
+def extract_average_transaction_per_block():
+    url = "https://api.blockchain.info/charts/n-transactions-per-block?timespan=30days&sampled=true&metadata=false&daysAverageString=1d&cors=true&format=json"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        values = data.get("values", [])
+        if values:
+            first_y = int(values[0].get("y"))
+            last_y = int(values[-1].get("y"))
+
+            print(f"Average transaction size per block last month {first_y}")
+            print(f"Average transaction size per block this month {last_y}")
+        else:
+            print("No values found in the response.")
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+
+# extract_unique_address_used()
+extract_average_transaction_per_block()
